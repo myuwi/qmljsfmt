a1546e1269bc7b4b74a5a516c38feb6aa27d4a7d
myuwi <myuwi@proton.me>
2026-08-16 22:29:47 +0300

    feat: read input from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read, Write};
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    io::stdout().write_all(input.as_bytes())
 }
